8bf4086c12f58fd1407db3a556929e1097387bdc
Robin <swesson92@gmail.com>
2019-08-06 14:47:42 +0200

    post function is now working

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-const express = require('express');
-const app = express();
 var cors = require('cors')
 const port = process.env.PORT || 5000;
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var mysql = require('mysql');
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.use(cors())
 
-
-// Displays that we are listening
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-//GET route for testing
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
-
-app.post('/create', (req, res) => {
-    res.send({express: 'det fungerar kanske?'})
-})
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -26,13 +22,32 @@ var connection = mysql.createConnection({
   database : 'testing'
 });
 
-//Connection to MySQL database
+//Connection/Disconnect to MySQL database
 connection.connect();
 
-//Test query to see if everything works as expected
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
 
-connection.end();
+// Displays that we are listening
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+app.post('/create', (req, res) => {
+    var author = req.body.author;
+    let message = req.body.message;
+    let headline = req.body.headline;
+
+    var sql = `INSERT INTO posts 
+            (
+                author, message, headline
+            )
+            VALUES
+            (
+                ?, ?, ?
+            )`;
+    connection.query(sql, [author, message, headline], function (err) {
+    if (err) {
+      throw err;
+    } else {
+       
+    }
+});
+});
